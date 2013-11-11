a7a0a48f7306a63a593295d803cc3afea8f9d1c8
Major1201 <major1201@gmail.com>
2013-11-11 21:18:11 +0800

    FileUtil

--- src/com/major/commons/util/FileUtil.java
+++ src/com/major/commons/util/FileUtil.java
@@ -1,5 +1,12 @@
 package com.major.commons.util;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * User: Minjie
  * Date: 13-9-20
@@ -11,4 +18,29 @@ public class FileUtil {
      * Don't let anyone instantiate this class.
      */
     private FileUtil() {}
+
+    /**
+     * List files and directories.
+     * @param directory target
+     * @return file list
+     */
+    public static List<File> listFilesAndDirs(File directory) {
+        if (directory == null || !directory.isDirectory() || !directory.exists())
+            return null;
+        return CollectionsUtil.arrayToList(directory.listFiles());
+    }
+
+    /**
+     * List files and directories recursively.
+     * @param directory target
+     * @return file list
+     */
+    public static List<File> listFilesAndDirsRecursive(File directory) {
+        if (directory == null || !directory.isDirectory() || !directory.exists())
+            return null;
+        List<File> list = new ArrayList<>(FileUtils.listFilesAndDirs
+                (directory, FileFilterUtils.trueFileFilter(), FileFilterUtils.directoryFileFilter()));
+        list.remove(0);
+        return list;
+    }
 }
